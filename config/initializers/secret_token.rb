2198dafdbd1d73387527e2094ee6e149df944aad
mayormcmatt <matthew.is.riding@gmail.com>
2014-04-10 15:36:16 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brewbeck::Application.config.secret_key_base = 'f911477c396377712731261c5c478cc9e29ac520aebe0808df17435c79f349b0755f0939d7e3ad5712bf225e6d61514887cf9588a40d7dd181925399918d8629'
