82c20ae3660a3ea6180b7d8b33968bc78575e559
mayormcmatt <matthew.is.riding@gmail.com>
2014-04-12 13:28:51 -0700

    Model relationships between breweries and beers input

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140412201612) do
+ActiveRecord::Schema.define(version: 20140412202714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140412201612) do
     t.integer  "ibu"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "brewery_id"
   end
 
   create_table "breweries", force: true do |t|
