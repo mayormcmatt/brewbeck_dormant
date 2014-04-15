53728be354d254041ad33c8b70a5e375f66824f5
mayormcmatt <matthew.is.riding@gmail.com>
2014-04-14 18:21:54 -0700

    Created brewery controller, writing method to populate that model with info

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Brewbeck::Application.routes.draw do
     devise_for :users
     resources :beer
+    resources :brewery
+
+    get "/brewery/poll_breweries", to: "brewery#poll_breweries"
+    post "/brewery/poll_breweries", to: "brewery#poll_breweries"
+
     root :to => "beer#index"
 end
