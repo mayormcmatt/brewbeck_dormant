d20942a6eb0ef811bd7060b49a59f6d8f9873b4d
mayormcmatt <matthew.is.riding@gmail.com>
2014-04-16 12:07:16 -0700

    Brewery polling done, just need to scale up when ready

--- app/controllers/poll_controller.rb
+++ app/controllers/poll_controller.rb
@@ -2,23 +2,22 @@ class PollController < ApplicationController
   def show
     page_number = 1
         # key = ENV['api_key']
-        until page_number == 5 # Set page 5 as temp. cut-off
+        until page_number == 5 # Set page 5 as temp. cut-off; real cut-off is 114
             # Get rid of that key when it's time to push to production
             response = Typhoeus.get("http://api.brewerydb.com/v2/locations?locality*&key=5a0a2875bbd1e7931736df7a5672f65f&p=#{page_number}")
             parsed = JSON.parse(response.response_body)
 
             parsed["data"].each do |info|
-                brewery_stats = Brewery.find_or_create_by(breweryID: info["id"])
-                brewery_stats.name = info["name"]
-                brewery_stats.address = info["streetAddress"]
-                brewery_stats.city = info["locality"]
-                brewery_stats.state = info["region"]
-                brewery_stats.zipcode = info["postalCode"]
-                brewery_stats.phone = info["phone"]
-                brewery_stats.website = info["website"]
-                brewery_stats.latitude = info["latitude"]
-                brewery_stats.longitude = info["longitude"]
-                brewery_stats.logo = info[""]
+                brewery_stats = Brewery.find_or_create_by(breweryID: info["id"],
+                    name: info["name"],
+                    address: info["streetAddress"],
+                    city: info["locality"],
+                    state: info["region"],
+                    zipcode: info["postalCode"],
+                    phone: info["phone"],
+                    website: info["website"],
+                    latitude: info["latitude"],
+                    longitude: info["longitude"])
             end
             page_number += 1
         end
